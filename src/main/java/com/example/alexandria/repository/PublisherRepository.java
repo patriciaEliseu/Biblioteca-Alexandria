a62e30dd6792fc73bbe0ff16a8e2effbd5d33c14
Patricia Eliseu <patyeliseu4@gmail.com>
2024-10-16 17:00:32 -0300

    add os repository

---
+++ src/main/java/com/example/alexandria/repository/PublisherRepository.java
@@ -0,0 +1,10 @@
+package com.example.alexandria.repository;
+
+import com.example.alexandria.entity.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+}
